b70848dabd48b79bfcb13da8aef74bd1ca22789b
Praveena Chandrapu <praveenachandram@gmail.com>
2018-06-10 14:03:15 -0700

    Fixes

--- stats.h
+++ stats.h
@@ -23,20 +23,19 @@
 /**
 *	@brief Prints the unsigned char array with its index and values explicitly
 *
-*	This function takes inputs as an unsigned char array and an integer which indicates the array size
+*	This function takes inputs as an unsigned char pointer array 
 *	This will print all the elements in the array based on the size of the array
 *
-* 	@param array The unsigned char array
-*	@param integer The integer to tell the size of the array
+* 	@param array The unsigned char pointer array
 *   @return no return value.
 */
-void print_array(unsigned char array[]);
+void print_array(unsigned char *array);
 
 
 /**
 *	@brief Swaps or interchanges the two char values passed as arguments.
 *
-*	This function takes inputs as two unsigned char values 
+*	This function takes input as two unsigned char values 
 *	This will swap the two char values
 *
 * 	@param  two unsigned char pointers, eg; a, b
